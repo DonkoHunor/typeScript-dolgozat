b34546bc3be6e74954820729cc54c4be9a4c69f1
Petrik Diak <diak@petrik.hu>
2023-11-10 09:53:03 +0100

    Setter megvalósítása a Staues osztályban

--- src/Statue.ts
+++ src/Statue.ts
@@ -3,49 +3,63 @@ import {Artwork} from './Artwork.ts';
 let hiba = document.getElementById('hiba') as HTMLElement;
 
 export class Statue implements Artwork{
-    title: string;
-    year: number;
-    price: number;
-    height: number;
+    title: string = "";
+    year: number = NaN;
+    price: number = NaN;
+    height: number = NaN;
     
     constructor(title:string, year: number, price: number, height: number){
-        
+        this.Title = title;
+        this.Year = year;
+        this.Price = price;
+        this.Height = height;
+    }
+
+    public set Title(title: string){
         if(title.trim().length == 0){
             hiba.textContent = "Hibás név(kötelező kitölteni)";
             throw Error("Üres név");
           }
-          if(!RegExp(/^[a-zA-Z,\s]+$/gm).test(title)){
+        if(!RegExp(/^[a-zA-Z,\s]+$/gm).test(title)){
             hiba.textContent = "Hibás név(érvénytelen karakter szerpel benne)";
             throw Error("Érvénytelen név");
-          }
-          if(isNaN(year)){
+        }
+        this.title = title;
+    }
+
+    public set Year(year: number){
+        if(isNaN(year)){
             hiba.textContent = "Hibás év(kötelező kitölteni)";
             throw Error("Üres év");
           }
-          if(year > new Date().getFullYear()){
+        if(year > new Date().getFullYear()){
             hiba.textContent = "Az év maximum értéke az idei év"
             throw Error("Túl nagy év érték");
-          }
-          if(isNaN(price)){
+        }
+        this.year = year;
+    }
+
+    public set Price(price: number){
+        if(isNaN(price)){
             hiba.textContent = "Hibás ár(kötelező kitölteni)";
             throw Error("Üres ár");
           }
-          if(price < 5){
+        if(price < 5){
             hiba.textContent = "Az ár minimum 5Ft"
             throw Error("Túl kicsi ár érték");
-          }
-          if(isNaN(height)){
+        }
+        this.price = price;
+    }
+
+    public set Height(height: number){
+        if(isNaN(height)){
             hiba.textContent = "Hibás magasság(kötelező kitölteni)";
             throw Error("Üres magasság");
           }
-          if(height < 15){
+        if(height < 15){
             hiba.textContent = "A magasság minimum 15cm"
             throw Error("Túl kicsi magasság érték");
-          }
-
-        this.title = title;
-        this.year = year;
-        this.price = price;
+        }
         this.height = height;
     }
 }
